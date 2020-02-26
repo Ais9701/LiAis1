98814420511588d62766775f388253730b4dba5c
Ais9701 <aysielu2016@gmail.com>
2020-02-26 08:56:03 +0500

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiAis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
